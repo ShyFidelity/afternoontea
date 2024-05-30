1ba1df9d62304fefaab559afa84475d48698fe10
Jillian FitzMaurice <info@jilliankayworks.com>
2024-05-30 11:14:11 -0600

    rm white wave

--- src/components/Work.js
+++ src/components/Work.js
@@ -68,13 +68,6 @@ export default function Work() {
       
 
 
-     <section>
-
-  <div class="wave wave1"></div>
-  <div class="wave wave2"></div>
-  <div class="wave wave3"></div>
-  <div class="wave wave4"></div>
-</section>
   
       {/* <h1>WORK</h1>
 
